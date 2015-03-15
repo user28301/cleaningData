6cb823e4b01004833944904b13f31f3c6f094e1a
user28301 <user28301@vbox.com>
2015-03-15 13:54:52 -0400

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,47 @@
+# Step 1 - Merge training and test data set
+
+subject <- rbind (read.table ("train/subject_train.txt"),
+                  read.table ("test/subject_test.txt"))
+x <- rbind (read.table ("train/X_train.txt"),
+            read.table ("test/X_test.txt"))
+y <- rbind (read.table ("train/y_train.txt"),
+            read.table ("test/y_test.txt"))
+
+# Step 2 - Extract columns for mean and std only
+
+features <- read.table ("features.txt")
+columnsWanted <- grep ("mean\\(\\)|std\\(\\)", tolower (features [,2]))
+x <- x [, columnsWanted]
+# also update feature names, so we don"t need columnsWanted
+features <- features [columnsWanted, 2]
+remove (columnsWanted)
+
+# Step 3 - Load in the activity and use it to transform y
+activity <- read.table ("activity_labels.txt")
+y <- merge (activity, y, by.x = 1, by.y = 1)
+activity <- y [,2]
+remove (y) # do not need that anymore
+
+# Step 4 - Transform feature names to make it more readable
+# first remove parentheses
+features <- gsub ("\\(", "", features)
+features <- gsub ("\\)", "", features)
+# change starting "t" to "timing.", and starting "f" to "freq."
+features <- gsub ("^t", "timing\\.", features)
+features <- gsub ("^f", "freq\\.", features)
+# change all "-" to "." for consistency
+features <- gsub ("-", "\\.", features)
+# and put a "." between words
+features <- gsub ("([a-z])([A-Z])", "\\1\\.\\2", features)
+# then convert all letter to lowercase
+features <- tolower (features)
+
+# Finally apply the transformed names to data
+names (x) <- features
+names (subject) <- c ("subject.id")
+remove (features)
+
+# Step 5 - create a tidy data set and write it out
+tidy <- cbind (subject, x, activity)
+remove (subject, x, activity)
+write.table (tidy, file = "tidy.data.txt", row.name = FALSE)
